c54122c8a4595b9d62bfe706bce7ac1ec70336bd
WebGL3D <OgreLandBob@gmail.com>
2017-05-09 21:51:23 -0700

    Add catalog.js

--- RobloxNpm/example.js
+++ RobloxNpm/example.js
@@ -6,6 +6,11 @@ Roblox.users.getByUserId(48103520).then(function (user) {
 	}).catch(function (e) {
 		console.error(e);
 	});
+	Roblox.users.getByUserId(3336955).then(function (user) {
+		console.log(user);
+	}).catch(function (e) {
+		console.error(e);
+	});
 }).catch(function (e) {
 	console.error(e);
 });
--- RobloxNpm/index.js
+++ RobloxNpm/index.js
@@ -1,12 +1,8 @@
-var $ = {
-	promise: require("./jquery.promise.js")({
-		defaultCacheRejectExpiry: 2000,
-		defaultCacheResolveExpiry: 5000
-	})
-};
+var $ = require("tixfactory.util")({});
 module.exports.auth = require("./auth.js")();
 module.exports.http = require("./http.js")(module.exports.auth);
 module.exports.users = require("./users.js")($, module.exports.http);
+module.exports.catalog = require("./catalog.js")($, module.exports.http);
 module.exports.inventory = require("./inventory.js")($, module.exports.http);
 
 // WebGL3D
