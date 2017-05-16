b728ed8e74d13fafae691113bbb7c6f08b5db491
WebGL3D <OgreLandBob@gmail.com>
2017-05-15 21:58:47 -0700

    add throttling to http client

--- RobloxNpm/index.js
+++ RobloxNpm/index.js
@@ -1,6 +1,18 @@
 var $ = require("tixfactory.util")({});
 var http = require("tixfactory.http");
 var httpClient = new http.client();
+
+httpClient.socketConfiguration({
+	getWritesPerSecond: function (host, port) {
+		if (host === "inventory.roblox.com") {
+			return 1000 / 60;
+		} else if (host === "search.roblox.com") {
+			return 80 / 60;
+		}
+		return 100;
+	}
+});
+
 module.exports.users = require("./users.js")($, httpClient);
 module.exports.catalog = require("./catalog.js")($, httpClient);
 module.exports.inventory = require("./inventory.js")($, httpClient);
